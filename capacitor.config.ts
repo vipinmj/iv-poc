005e1d49d11e274a58433862aa07ba1bc09d6aba
Vipin Jose <vipin.mj@gmail.com>
2021-09-22 00:34:04 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.gettingstartedivangular',
+  appName: 'getting-started-iv-angular',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
